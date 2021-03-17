fdbd7489578f602a1468295c9af90290e7c573c6
Aditya-Shrivastava <39914834+Aditya-Shrivastava@users.noreply.github.com>
2021-03-17 21:54:28 +0530

    add user routes

--- server/index.js
+++ server/index.js
@@ -4,21 +4,58 @@ const socketio = require('socket.io');
 const http = require('http');
 const router = require('./router');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+		credentials: true,
+	},
+});
+
+app.use(cors());
+app.use(router);
 
 io.on('connection', (socket) => {
-	console.log('We have a new conenction :)');
+	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
+
+		if (error) return callback(error);
+
+		socket.emit('message', {
+			user: 'admin',
+			text: `${user.name}, welcome to the room ${user.room}`,
+		});
+
+		socket.broadcast.to(user.room).emit('message', {
+			user: 'admin',
+			text: `${user.name} has joined`,
+		});
+
+		socket.join(user.room);
+
+		callback();
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+		// console.log(user);
+		console.log(socket.id);
+
+		io.to(user.room).emit('message', { user: user.name, text: message });
+
+		callback();
+	});
 
 	socket.on('disconnect', () => {
 		console.log('User left :(');
 	});
 });
-
-app.use(router);
 
 server.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
--- server/router.js
+++ server/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*');
 	res.send('Server is up and running');
 });
 
