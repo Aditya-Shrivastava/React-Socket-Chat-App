029ec91fe1b00cd177068175b7d39853290ecec1
Aditya-Shrivastava <39914834+Aditya-Shrivastava@users.noreply.github.com>
2021-03-17 21:54:08 +0530

    add socket client

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+	const [name, setName] = useState('');
+	const [room, setRoom] = useState('');
+	const [messages, setMessages] = useState([]);
+	const [message, setMessage] = useState('');
+	const ENDPOINT = 'http://localhost:5000';
+
+	useEffect(() => {
+		const { name, room } = queryString.parse(location.search);
+
+		socket = io(ENDPOINT, {
+			transports: ['websocket', 'polling', 'flashsocket'],
+		});
+
+		setName(name);
+		setRoom(room);
+
+		socket.emit('join', { name, room }, () => {});
+
+		return () => {
+			socket.emit('disconnect');
+
+			socket.off();
+		};
+	}, [ENDPOINT, location.search]);
+
+	useEffect(() => {
+		socket.on('message', (message) => {
+			setMessages([...messages, message]);
+		});
+	}, [messages]);
+
+	const sendMessage = (e) => {
+		e.preventDefault();
+
+		if (message) {
+			socket.emit('sendMessage', message, () => {
+				setMessage('');
+			});
+		}
+	};
+
+	console.log(messages, message);
+
 	return (
 		<div className='flex justify-center items-center bg-blue-400 h-screen w-100'>
-			<h1 className='text-9xl font-black uppercase'>Chat</h1>
+			<div className='w-full'>
+				<input
+					className='w-full h-12 px-4 text-xl focus:outline-none focus:ring focus:border-blue-200 bg-gray-200 mb-3'
+					placeholder='Room'
+					type='text'
+					onChange={(e) => setMessage(e.target.value)}
+					value={message}
+					onKeyPress={(e) =>
+						e.key === 'Enter' ? sendMessage(e) : null
+					}
+				/>
+			</div>
 		</div>
 	);
 };
--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -6,24 +6,26 @@ const Join = () => {
 	const [room, setRoom] = useState('');
 
 	return (
-		<div className='flex h-screen w-100 justify-center items-center bg-green-400'>
+		<div className='flex h-screen w-100 justify-center items-center bg-black'>
 			<div
-				className='w-4/5
-			 h-1/2 md:w-3/5 lg:w-1/2 bg-white rounded shadow-lg px-8 py-8 font-mono flex justify-evenly flex-col'
+				className='w-2/5
+			  rounded font-mono flex justify-cente items-center flex-col'
 			>
-				<h1 className='font-black text-4xl uppercase mb-3'>Join</h1>
-				<div>
+				<h1 className='font-semibold text-4xl mb-6 text-white text-center border-b w-full'>
+					Join
+				</h1>
+				<div className='w-full'>
 					<input
-						className='w-full h-12 px-4 text-xl focus:outline-none rounded-md focus:ring focus:border-blue-200 bg-gray-200'
+						className='w-full h-12 px-4 text-xl focus:outline-none focus:ring focus:border-blue-200 bg-gray-200 mb-3'
 						placeholder='Name'
 						type='text'
 						onChange={(e) => setName(e.target.value)}
 						value={name}
 					/>
 				</div>
-				<div>
+				<div className='w-full'>
 					<input
-						className='w-full h-12 px-4 text-xl focus:outline-none rounded-md focus:ring focus:border-blue-200 bg-gray-200'
+						className='w-full h-12 px-4 text-xl focus:outline-none focus:ring focus:border-blue-200 bg-gray-200 mb-3'
 						placeholder='Room'
 						type='text'
 						onChange={(e) => setRoom(e.target.value)}
@@ -31,13 +33,14 @@ const Join = () => {
 					/>
 				</div>
 				<Link
+					className='w-full'
 					onClick={(e) =>
 						!name || !room ? e.preventDefault() : null
 					}
 					to={`/chat?name=${name}&room=${room}`}
 				>
 					<button
-						className='p-2 w-40 rounded-md shadow-md bg-blue-500 text-white font-semibold text-xl hover:bg-green-400 transition duration-500 ease-in-out hover:shadow-2xl'
+						className='p-2 mt-2 bg-blue-700 text-white font-regular text-xl hover:bg-green-400 transition duration-500 ease-in-out w-full'
 						type='submit'
 					>
 						Sign In
